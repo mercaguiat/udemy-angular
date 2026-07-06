092061074d2e8da3a7feed5ab2d576ff44d315d9
Merci <160075925+mercaguiat@users.noreply.github.com>
2026-07-06 20:36:37 +0800

    #27. Managing State & Changing Data

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -19,7 +19,8 @@ export class UserComponent {
   }
 
   onSelectUser() {
-    console.log("Click!");
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser = DUMMY_USERS[randomIndex];
   }
 
 }
